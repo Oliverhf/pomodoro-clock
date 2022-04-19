bc78749721817a289d361ff15466e414ee75fb69
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-04-19 20:06:32 -0300

    feat: init pomodoro components, Button and SetPomodoro

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SetPomodoro from "./components/SetPomodoro/SetPomodoro";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Pomodoro</h1>
+      <small>be productive the right way.</small>
+      <SetPomodoro />
+    </div>
+  );
+}
+
+export default App;
